eb4c0215bb317da8a2952b2bb09ce31a83fc3a90
SantiTorija <torija.santiago@gmail.com>
2025-06-18 15:15:04 -0300

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -19,7 +19,7 @@ module.exports = (sequelize) => {
         type: DataTypes.STRING,
         allowNull: false,
       },
-      phone: {
+      email: {
         type: DataTypes.STRING,
         allowNull: false,
       },
