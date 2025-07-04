e2798838891f2722733093b0f5d85c9f9d9c39cc
SantiTorija <torija.santiago@gmail.com>
2025-07-04 13:47:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,8 @@ const dbInitialSetup = require("./dbInitialSetup");
 const app = express();
 const PORT = process.env.PORT;
 
-app.use(express.json());
-
 app.use(cors());
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 routes(app);
