2986cc7447e86104751319029931d18e162822b4
SantiTorija <torija.santiago@gmail.com>
2025-07-04 14:19:20 -0300

    Update emailService.js

--- services/emailService.js
+++ services/emailService.js
@@ -8,13 +8,13 @@ const confirmationEmail = async ({ to, date, time, total }) => {
   const subject = "Confirmación de reserva - Lavajato";
   const text = `¡Hola! Tu reserva ha sido confirmada. Por favor, ten en cuenta que tenemos un margen de 30 minutos de consideración, luego de ese plazo perderías tu turno. Fecha: ${date}, hora: ${time}, total: ${total}`;
   const html = `
-    <h2><strong>¡Reserva confirmada!</strong></h2>
-    <p>Hola, tu reserva ha sido confirmada.</p>
+    <h1><strong>¡Reserva confirmada!</strong></h1>
+    <strong>Hola, tu reserva ha sido confirmada.</strong>
     <p>Atención: Por favor, ten en cuenta que tenemos un margen de 30 minutos de consideración. Luego de ese plazo perderías tu turno.</p>
-    <br/>
     <p><strong>Fecha:</strong> ${date}</p>
     <p><strong>Hora:</strong> ${time}</p>
-    <p><strong>Total:</strong> ${total} <strong>A pagar en el local</strong></p>
+    <p><strong>Total:</strong> UY$${total} <strong>A pagar en el local</strong></p>
+    <h2>¡Muchas gracias por la confianza!</h2>
   `;
 
   const msg = {
