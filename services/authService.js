f0945077f1ae17f20e1cd3aee5e517b9ab95f5b5
SantiTorija <torija.santiago@gmail.com>
2025-07-29 18:16:59 -0300

    Update authService.js

--- services/authService.js
+++ services/authService.js
@@ -29,7 +29,7 @@ async function loginUser(email, password) {
   const token = jwt.sign(
     { id: user.id, role: user.role },
     process.env.JWT_SECRET,
-    { expiresIn: process.env.JWT_EXPIRES_IN || "1h" }
+    { expiresIn: process.env.JWT_EXPIRES_IN || "15h" }
   );
 
   return { user: userData, token };
